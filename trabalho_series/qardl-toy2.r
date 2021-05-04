e7495995788492b3ca81f79235620d238cb6d712
Tais Bellini <tais38@gmail.com>
2021-05-04 08:21:05 -0300

    First code running:

---
+++ trabalho_series/qardl-toy2.r
@@ -0,0 +1,176 @@
+# This code simulates a sample path of size T from a simple QARDL(1,1) model:
+# Q(𝜏| ℱ[t-1]) = α₀(𝜏) + α₁(𝜏)*Y[t-1] + θ₁(𝜏)*Z[t-1]
+
+# set.seed(1)
+library(quantreg)
+library(conquer)
+library(forecast)
+source('utils.R')
+
+tau.grid = seq(from=.01,to=.99, by=.02)
+
+# Value of the conditional quantile function at three
+# vertices of the unit square (v10, v01, v00 and v11)
+# Since v11 = v10 + v01 - v00 we can choose, say, v10 and v01
+# freely and then choose v00 restricted with the restriction that
+# v10 + v01 - v00 is non-decreasing and has its range contained
+# in the unit interval [0,1]. If the functions are chosen 
+# to be differentiable quantile functions, it is sufficient 
+# then to choose v00 so that
+# v10' + v01' \ge v00'
+# or, equivalently, log(v10' + v01')-log(v00')\ge0
+# The function qdbplot below allows us to investigate
+# if this inequality holds when v00, v10 and v01 
+# are chosen to be Beta quantile functions: 
+qdbplot = function(a10,b10,a01,b01,a00,b00){
+tau = seq(from=0.00001,to=.99999,by=.00001)
+v00prime = 1/dbeta(qbeta(tau,a00,b00),a00,b00)
+v10prime = 1/dbeta(qbeta(tau,a10,b10),a10,b10)
+v01prime = 1/dbeta(qbeta(tau,a01,b01),a01,b01)
+plot(tau, log(v10prime+v01prime)-log(v00prime), type = 'l'); abline(h=0, col='red')
+}
+
+
+# A function for visualizing the Beta densities:
+dbplot = function(a,b){
+ tau = seq(from=0,to=1,by=.01)
+ plot(tau, dbeta(tau,a,b))
+}
+
+# Parameters for the Beta quantile functions v10, v01 and v00
+{a10 = 1; b10 = 3; a01 = 5; b01 = 1; a00 = 3; b00 = 1}
+qdbplot(a10,b10, a01,b01, a00,b00)
+
+# Conditional quantile function of Y[t] given Y[t-1] = 1 and X[t-1]=0
+v10 = function (tau) qbeta(tau, a10,b10)
+# Conditional density of Y[t] given Y[t-1] = 1 and X[t-1]=0
+dbplot(a10,b10)
+
+# Conditional quantile function of Y[t] given Y[t-1] = 0 and X[t-1]=1
+v01 = function(tau) qbeta(tau,a01,b01)
+# Conditional density of Y[t] given Y[t-1] = 0 and X[t-1]=1
+dbplot(a01,b01)
+
+# Conditional quantile function of Y[t] given Y[t-1] = 0 and X[t-1]=0
+v00 = function(tau) qbeta(tau,a00,b00)
+# Conditional density of Y[t] given Y[t-1] = 0 and X[t-1]=0
+dbplot(a00,b00)
+
+# Conditional quantile function of Y[t] given Y[t-1] = 1 and X[t-1]=1
+v11 = function(tau) v10(tau) + v01(tau) - v00(tau)
+# Plot of v11 (must be nondecreasing and with range contained in [0,1])
+plot(tau.grid, v11(tau.grid))
+# Conditional density of Y[t] given Y[t-1] = 1 and X[t-1]=1
+q11 = function(tau) {
+1/dbeta(v10(tau),a10,b10) + 1/dbeta(v01(tau),a01,b01) - 1/dbeta(v00(tau),a00,b00)
+}
+plot(v11(tau.grid), 1/q11(tau.grid), xlim=c(0,1))
+
+# Functional parameters for the quantile regression equation
+alpha0 = v00
+alpha1 = function(tau) v10(tau) - v00(tau)
+theta1 = function(tau) v01(tau) - v00(tau)
+
+# Quantile function of Y given ℱ[t-1]
+Q = function(tau,Y.current,Z.current){
+ alpha0(tau) + alpha1(tau)*Y.current + theta1(tau)*Z.current
+}
+
+# Simulating the sample paths:
+
+# Arbitrary starting point (Y0,Z0)
+Y0 = runif(1)
+Z0 = runif(1)
+
+Y = Z = numeric()
+Z.current = Z0
+Y.current = Y0
+T = 10001
+for (t in 1:T){
+# Simulates Y[t] given ℱ[t-1] using the Fundamental Theorem of Simulation
+ Y[t] = Q(runif(1), Y.current, Z.current)
+ 
+ Z[t] = Q(runif(1), Z.current, Y.current)
+ # Q(runif(1), Z.current, Y.current)
+ # Z[t] = runif(1,max(Y[t]-.01,0),min(Y[t]+.01,1))
+ 
+ Z.current = Z[t]
+ Y.current = Y[t]
+}
+
+Y = Y[-1]
+Z = Z[-1]
+T = length(Y)
+acf(Y, lwd=16, lend=3, col='gray')
+pacf(Y, lwd=16, lend=3, col='gray')
+
+plot(Y[2:T]~Y[1:(T-1)], pch=16, col=rgb(0,0,0,.4))
+plot(Y[2:T]~Z[1:(T-1)], pch=16, col=rgb(0,0,0,.4))
+hist(Y, border=NA, breaks="FD")
+ts.plot(Y)
+
+# True and estimated functional parameters
+
+# quantile regression
+Yvec = Y[2:T]
+Xmat = cbind(Y[1:(T-1)], Z[1:(T-1)])
+qrfit = rq(Yvec~Xmat, tau = tau.grid)
+plot(tau.grid, alpha0(tau.grid), type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, coef(qrfit)[1,], lwd=2, col = rgb(0,0,0,.7))
+
+plot(tau.grid, alpha1(tau.grid), type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, coef(qrfit)[2,], lwd=2, col = rgb(0,0,0,.7))
+
+plot(tau.grid, theta1(tau.grid), type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, coef(qrfit)[3,], lwd=2, col = rgb(0,0,0,.7))
+
+# kernel estimation
+conquerfit = sapply(tau.grid, function(tau) conquer(Xmat,Yvec,tau=tau)$coeff)
+plot(tau.grid, alpha0(tau.grid), type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, conquerfit[1,], lwd=2, col = rgb(0,0,0,.7))
+# 
+plot(tau.grid, alpha1(tau.grid), type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, conquerfit[2,], lwd=2, col = rgb(0,0,0,.7))
+# 
+plot(tau.grid, theta1(tau.grid), type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, conquerfit[3,], lwd=2, col = rgb(0,0,0,.7))
+
+
+# global coefficients
+phi = phi_generator(3, tau.grid)
+Xmat1 = cbind(rep(1,nrow(Xmat)), Xmat)
+globalfit = global_qr(taus = tau.grid, phi = phi, X = Xmat1, y = Yvec)
+
+plot(tau.grid, alpha0(tau.grid), type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, globalfit$bhat[1,], lwd=2, col = rgb(0,0,0,.7))
+
+plot(tau.grid, alpha1(tau.grid), type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, globalfit$bhat[2,], lwd=2, col = rgb(0,0,0,.7))
+
+plot(tau.grid, theta1(tau.grid), type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, globalfit$bhat[3,], lwd=2, col = rgb(0,0,0,.7))
+
+# ssr comparison
+ssrqr = cbind((alpha0(tau.grid) - coef(qrfit)[1,])^2, (alpha1(tau.grid) - coef(qrfit)[2,])^2, (theta1(tau.grid) - coef(qrfit)[3,])^2)
+ssrconquer = cbind((alpha0(tau.grid) - conquerfit[1,])^2, (alpha1(tau.grid) - conquerfit[2,])^2, (theta1(tau.grid) - conquerfit[3,])^2)
+ssrglobal = cbind((alpha0(tau.grid) - globalfit$bhat[1,])^2, (alpha1(tau.grid) - globalfit$bhat[2,])^2, (theta1(tau.grid) - globalfit$bhat[3,])^2)
+
+plot(tau.grid, ssrqr[,1], type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, ssrconquer[,1], lwd=2, col = rgb(0,1,0,.7))
+lines(tau.grid, ssrglobal[,1], lwd=2, col = rgb(0,0,0,.7))
+
+plot(tau.grid, ssrconquer[,2], type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, ssrglobal[,2], lwd=2, col = rgb(0,0,0,.7))
+
+plot(tau.grid, ssrconquer[,3], type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, ssrglobal[,3], lwd=2, col = rgb(0,0,0,.7))
+
+
+ssrqr_mean = apply(ssrqr, 1, mean)
+ssrconquer_mean = apply(ssrconquer, 1, mean)
+ssrglobal_mean = apply(ssrglobal, 1, mean)
+plot(tau.grid, ssrconquer_mean, type = 'l', col='blue', lwd=2, lty='dotted')
+lines(tau.grid, ssrglobal_mean, lwd=2, col = rgb(0,0,0,.7))
+lines(tau.grid, ssrqr_mean, lwd=2, col = rgb(0,1,0,.7))
+
+cbind(mean(ssrqr),mean(ssrconquer), mean(ssrglobal))
---
+++ trabalho_series/utils.R
@@ -0,0 +1,214 @@
+#### Utils trabalho series ####
+#### Loading required packages ####
+
+if(!require(quantreg)) install.packages('quantreg')
+if(!require(CVXR)) install.packages('CVXR')
+
+library(quantreg)
+library(CVXR)
+
+
+#### Objective function ####
+
+R = function(a, Y, X, TAUS, phi){
+  B = a%*%phi # candidate optimizer
+  R.eval = (2*TAUS-1)*(Y-X%*%B) + abs(Y-X%*%B)
+  return(sum(R.eval)) # mean of the preceding objective functions
+}
+
+
+#### Simulation function #### 
+### Run one experiment estimating using the traditional qr estimator and the proposed one
+### Save ahat and bhat found
+
+penalty = function(a){
+  sum(abs(a))
+}
+
+#todo - optimize via optim
+global_qr = function(taus = c(0.5), phi = matrix(), X = matrix(), y = c(), lambda = 0){
+  # Get parameters
+  M = length(taus)
+  L = nrow(phi)
+  D = ncol(X)
+  N = nrow(X)
+  
+  Y = matrix(rep(y,M),N,M)
+  TAUS = matrix(rep(taus,N),N,M, byrow = TRUE)
+  
+  a = Variable(D,L)
+  objective = R(a, Y, X, TAUS, phi) + lambda*penalty(a)
+  problem = Problem(Minimize(objective))
+  result = solve(problem)
+  ahat = result$getValue(a)
+  bhat = ahat%*%phi
+  
+  return(list(
+    "ahat" = ahat,
+    "bhat" = bhat
+  ))
+}
+
+bhatQR = function(taus = c(0.5), X = matrix(), y = c()){
+  bhat = rq(y~X[,-1],taus)$coef
+  return(list("bhat" = bhat))
+} 
+
+R_univ = function(a = a, taus = taus, y = y){ # objective function
+  N = length(y)
+  M = length(taus)
+  
+  b = phi%*%a # candidate optimizer
+  foo = sapply(1:M, function(m){
+    argeval = y-b[m]
+    # objective function corresponding to quantile level = taus[m]:
+    sum((2*taus[m]-1)*(argeval) + abs(argeval))/N
+    # sum((taus[m]-1)*(argeval) + argeval*pnorm(argeval,sd=h) + h^2*dnorm(argeval,sd=h))/N
+  }
+  )
+  return(sum(foo)/M) # mean of the preceding objective functions
+}
+
+global_qr_uni = function(phi = phi){
+  L = nrow(phi)
+  ahat = optim(rep(1,L),R)$par
+  bhat = phi%*%ahat
+  return (list(
+    "ahat" = ahat,
+    "bhat" = bhat
+  ))
+}
+
+
+
+
+
+#### MC function ####
+### Run a MC experiment estimating using the traditional qr estimator and the proposed one
+### Save MSE for each iteration and evaluate results
+### Save Bhat found for each
+
+# nrepl: number of monte carlo replications
+# N: sample size
+# D: covariables 
+# M: tau grid size
+
+run_mc = function(nrepl = 200, N = 100, taus = c(0.5), phi = matrix(), beta = c(1), lambda = 0, sd_error = 0.1){
+  
+  # Get parameters
+  M = length(taus)
+  D = length(beta)
+  L = nrow(phi)
+  
+  # Initialize response variables
+  Bhat_list = list()
+  BhatQR_list = list()
+  mse_Bhat_list = list()
+  mse_BhatQR_list = list()
+  
+  for (i in 1:D){
+    Bhat_list[[i]] = matrix(0,nrepl,M)
+    BhatQR_list[[i]] = matrix(0,nrepl,M)
+    mse_Bhat_list[[i]] = matrix(0,nrepl,M)
+    mse_BhatQR_list[[i]] = matrix(0,nrepl,M)
+  }
+  
+  # Run simulation 
+  for (i in 1:nrepl){
+    
+    X = matrix(runif(N*D),N,D)
+    X[,1] = 1
+    Xbar = colMeans(X)
+    
+    y = X%*%beta + rnorm(N,sd=sd_error)
+    
+    Y = matrix(rep(y,M),N,M)
+    TAUS = matrix(rep(taus,N),N,M, byrow = TRUE)
+    
+    bhatQR = rq(y~X[,-1],taus)$coef
+    
+    a = Variable(D,L)
+    penalty = function(a) sum(abs(a))
+    objective = R(a, Y, X, TAUS) + lambda*penalty(a)
+    problem = Problem(Minimize(objective))
+    result = solve(problem)
+    if(result$status == 'solver_error') next
+    ahat = result$getValue(a)
+    bhat = ahat%*%phi
+    
+    for(k in 1:D){
+      Bhat_list[[k]][i,] = bhat[k,]
+      BhatQR_list[[k]][i,] = bhatQR[k,]
+      mse_Bhat_list[[k]][i,] = (beta[k] - bhat[k,])^2
+      mse_BhatQR_list[[k]][i,] = (beta[k] - bhatQR[k,])^2
+    }
+  }
+  
+  return(list(
+    "bhat" = Bhat_list,
+    "bhatQR" = BhatQR_list,
+    "mse_bhat" = mse_Bhat_list,
+    "mse_bhatQR" = mse_BhatQR_list
+  ))
+}
+
+
+#### Multiplot ####
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+#### Helper functions ####
+
+# Generate the phi matrix based on the taus grid and number of basis vectors
+phi_generator = function(L, taus){
+  M = length(taus)
+  phi = matrix(0,L,M) # matrix to store basis vectors
+  colnames(phi) = as.character(taus)
+  
+  # finite dimensional analogue of Legendre polynomials
+  phi[1,] = rep(1,M)
+  phi[1,] = phi[1,]/sqrt(sum(phi[1,]^2))
+  phi[2,] = lm(taus~0+phi[1,])$res
+  phi[2,] = phi[2,]/sqrt(sum(phi[2,]^2))
+  if (L>=3) {
+    for (ell in 3:L){
+      phi[ell,] = lm(taus^(ell-1)~t(phi[2:ell,]))$res
+      phi[ell,] = phi[ell,]/sqrt(sum(phi[ell,]^2))
+    }
+  }
+  return(phi)
+}
+
